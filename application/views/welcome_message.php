feb69b369adb267d2851a0c5aa066f1936a840a5
gugusd999 <ggsd09031997@gmail.com>
2021-03-26 06:17:08 +0700

    update 21-03-26 06:17:08

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -219,12 +219,12 @@
           assets: images
         },
         storageManager: {
-          type: 'remote',
+          type: null,
           stepsBeforeSave: 3,
-        autosave: true,         // Store data automatically
-        autoload: true,
-          urlLoad: '<?= site_url() ?>/admin/page/laodpage/'+idbuuild,
-          urlStore: '<?= site_url() ?>/admin/page/savepages/'+idbuuild,
+          // autosave: true,         // Store data automatically
+          // autoload: true,
+          // urlLoad: '<?= site_url() ?>/admin/page/laodpage/'+idbuuild,
+          // urlStore: '<?= site_url() ?>/admin/page/savepages/'+idbuuild,
           // ContentType: 'application/json',
           // For custom parameters/headers on requests
           //params: { _some_token: '....' },
